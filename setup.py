52baa86965a6a28c5de1e522915d2960bf12d01e
Gio at UMCU <github@gpiantoni.com>
2019-12-19 10:17:57 +0100

    use function with path to sqlite

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xelo2',
+    version='0.1',
+    description='Database GUI',
+    url='https://github.com/gpiantoni/xelo2',
+    author="Gio Piantoni",
+    author_email='xelo2@gpiantoni.com',
+    license='GPLv3',
+    classifiers=[
+        'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='database qt',
+    packages=find_packages(),
+    install_requires=[
+        ],
+    entry_points={
+        'console_scripts': [
+            'xelo2=xelo2.gui.main:main',
+        ],
+    },
+)
---
+++ xelo2/gui/main.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from argparse import ArgumentParser
+
+from PyQt5.QtWidgets import QApplication
+
+from .interface import Interface
+from ..model import open_database
+
+
+app = QApplication([])
+
+
+def main():
+
+    parser = ArgumentParser(prog='open xelo2 database')
+    parser.add_argument(
+        'sqlite',
+        nargs='?',
+        help='path to ')
+    args = parser.parse_args()
+
+    sqlite = Path(args.sqlite).resolve()
+    sql, cur = open_database(sqlite)
+
+    w = Interface(cur)
+    app.exec()
+    sql.close()
+
+
+if __name__ == '__main__':
+
+    main()
