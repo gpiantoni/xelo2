42616dc28d904ddc631bcebfe0b4335b0caf9258
Gio at UMCU <github@gpiantoni.com>
2020-02-24 14:53:23 +0100

    add umcu for bair project

--- xelo2/bids/bair_project.py
+++ xelo2/bids/bair_project.py
@@ -8,6 +8,35 @@ TASK_TYPES = {
     }
 
 
+def make_bair_compatible(bids_dir):
+
+    add_umcu_to_sub_ses(bids_dir)
+    add_info_to_participants(bids_dir)
+
+
+def add_umcu_to_sub_ses(bids_dir):
+
+    for tsv_file in bids_dir.glob('**/*.tsv'):
+        with tsv_file.open() as f:
+            txt = f.read()
+        txt = txt.replace('sub-', 'sub-umcu')
+        txt = txt.replace('ses-', 'ses-umcu')
+        with tsv_file.open('w') as f:
+            f.write(txt)
+
+    for subj_path in bids_dir.glob('sub-*'):
+        new_subj_path = subj_path.parent / (subj_path.name.replace('sub-', 'sub-umcu'))
+        subj_path.rename(new_subj_path)
+
+        for sess_path in new_subj_path.glob('ses-*'):
+            new_sess_path = sess_path.parent / (sess_path.name.replace('ses-', 'ses-umcu'))
+            sess_path.rename(new_sess_path)
+
+    for old_file in bids_dir.glob('**/*.*'):
+        new_name = old_file.name.replace('sub-', 'sub-umcu').replace('ses-', 'ses-umcu')
+        old_file.rename(old_file.parent / new_name)
+
+
 def add_info_to_participants(bids_path):
     """Add some additional information to participants"""
     tsv_file = bids_path / 'participants.tsv'
