7d13fbc1cc87375a65ac365fda6833f80f828923
Gio at UMCU <github@gpiantoni.com>
2020-03-16 15:54:53 +0100

    scanphysiolog

---
+++ xelo2/bids/io/pulse_and_resp_scanner.py
@@ -0,0 +1,31 @@
+from pandas import read_csv
+
+NAMES = [
+    'v1raw',
+    'v2raw',
+    'v1',
+    'v2',
+    'ppu',
+    'resp',
+    'gx',
+    'gy',
+    'gz',
+    'mark']
+
+
+def parse_scanner_physio(physio_log):
+    physio = read_csv(
+        physio_log,
+        delim_whitespace=True,
+        names=NAMES,
+        comment='#')
+
+    physio = physio.loc[:, (physio != 0).any(axis=0)]
+
+    hdr = {
+        "SamplingFrequency": 512,
+        "StartTime": 0,
+        "Columns": list(physio.columns),
+        }
+
+    return physio, hdr
--- xelo2/bids/physio.py
+++ xelo2/bids/physio.py
@@ -4,6 +4,7 @@ from json import dump
 from bidso.utils import replace_extension, add_underscore, remove_underscore
 
 from .io.dataglove import parse_dataglove_log
+from .io.pulse_and_resp_scanner import parse_scanner_physio
 
 
 def convert_physio(rec, data_name):
@@ -13,6 +14,15 @@ def convert_physio(rec, data_name):
         if file.format == 'dataglove':
             rec_name = 'dataglove'
             tsv, hdr = parse_dataglove_log(file.path)
+
+        elif file.format == 'scanphysiolog':
+            rec_name = 'resp'
+            tsv, hdr = parse_scanner_physio(file.path)
+
+        else:
+            return
+
+        hdr['StartTime'] = rec.onset
 
     physio_tsv = add_underscore(base_name, f'rec-{rec_name}_physio.tsv.gz')
     _write_physio(tsv, physio_tsv)
