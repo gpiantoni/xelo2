a48fdeb14928a38d093463e77129ce7df5c6c809
Gio on ArchXPS <github@gpiantoni.com>
2020-12-31 20:59:50 +0100

    qtbot

--- tests/test_01_database.py
+++ tests/test_01_database.py
@@ -3,7 +3,7 @@ from xelo2.database.open_db import open_database
 from .paths import DB_ARGS
 
 
-def test_open():
+def test_open(qtbot):
     db = open_database(**DB_ARGS)
     n_tables = 32
     assert len(db.tables()) == n_tables
