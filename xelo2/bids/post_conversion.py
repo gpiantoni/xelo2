bae102120b6f495a9927140a2d5e79a2ded339c3
Gio at UMCU <github@gpiantoni.com>
2021-02-24 11:40:30 +0100

    post conversion

---
+++ xelo2/bids/post_conversion.py
@@ -0,0 +1,42 @@
+from itertools import chain
+
+
+def anonymize_subjects(bids_dir, subj_code):
+    """
+
+    Parameters
+    ----------
+    bids_dir : path
+        path to bids folder
+    subj_code : dict
+        keys are current subject names and values are new subject names, f.e.
+        {'sub-oldname': 'sub-001'}
+    """
+    TEXT_FILES = chain(
+        bids_dir.glob('**/*.tsv'),
+        bids_dir.glob('**/*.vhdr'),
+        bids_dir.glob('**/*.vmrk'),
+        bids_dir.glob('**/*.json'),
+        )
+
+    for tsv_file in TEXT_FILES:
+        with tsv_file.open() as f:
+            txt = f.read()
+        for old, new in subj_code.items():
+            txt = txt.replace(old, new)
+        with tsv_file.open('w') as f:
+            f.write(txt)
+
+    for subj_path in bids_dir.glob('sub-*'):
+        txt = subj_path.name
+        for old, new in subj_code.items():
+            txt = txt.replace(old, new)
+
+        new_subj_path = subj_path.parent / txt
+        subj_path.rename(new_subj_path)
+
+    for old_file in bids_dir.glob('**/*.*'):
+        txt = old_file.name
+        for old, new in subj_code.items():
+            txt = txt.replace(old, new)
+        old_file.rename(old_file.parent / txt)
